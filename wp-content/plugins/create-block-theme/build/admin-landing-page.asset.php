c60e16b11ba820811bd64c02ffbcdb3397be0cb0
agat dev <agathe@agat.dev>
2024-10-01 16:34:34 +0200

    starter commit with slider homepage

--- wp-content/plugins/create-block-theme/build/admin-landing-page.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-blob', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd104b00c944a6cdc970b');
